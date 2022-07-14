e6bdd284b63ef5d9a9c490625d14191d3a97bdcf
Olga Bystrova <OVlBystrova@sberbank.ru>
2022-07-14 20:11:54 +0300

    add tests for formatters

---
+++ tests/test_formatters.py
@@ -0,0 +1,178 @@
+from typing import List, Tuple, Dict
+from unittest import TestCase
+
+from parameterized import parameterized
+
+from src.formatters.Answer import AnswerFormatter
+from src.formatters import (
+    NERTaskFormatter,
+    EntityExtractTaskFormatter,
+    EntityTypeTaskFormatter
+)
+
+
+class TestReader(TestCase):
+    maxDiff = None
+
+    @parameterized.expand([
+        ("SOCCER - JAPAN GET LUCKY WIN , CHINA IN SURPRISE DEFEAT .", [(9, 14, "LOC"), (31, 36, "LOC")], "en",
+         "JAPAN is a LOC, CHINA is a LOC."),
+        ("Bitar pulled off fine saves whenever they did .", [(0, 5, "PER")], "en",
+         "Bitar is a PER.")
+    ])
+    def test_format_answer_from_spans(
+            self,
+            context: str,
+            entity_spans: List[Tuple[int, int, str]],
+            language: str, output_true: str
+    ):
+
+        answer_pred = AnswerFormatter.from_spans(
+            context=context,
+            entity_spans=entity_spans,
+            language=language
+        )
+
+        self.assertEqual(
+            answer_pred,
+            output_true
+        )
+
+    @parameterized.expand([
+        ("AL-AIN , United Arab Emirates 1996-12-06", [(0, 6, "LOC"), (9, 29, "LOC")], "ru")
+    ])
+    def test_raise_error_when_wrong_language(
+            self,
+            context: str,
+            entity_spans: List[Tuple[int, int, str]],
+            language
+    ):
+
+        self.assertRaises(
+            ValueError,
+            AnswerFormatter.from_spans,
+            context, entity_spans, language
+        )
+
+    @parameterized.expand([
+        ({"LOC": ["JAPAN"], "PER": ["CHINA"]}, "en",
+         "JAPAN is a LOC, CHINA is a PER."),
+        ({"PER": ["Bitar"]}, "en",
+         "Bitar is a PER.")
+    ])
+    def test_format_answer_from_values(
+            self,
+            entity_values: Dict[str, List[str]],
+            language: str,
+            output_true: str
+    ):
+
+        answer_pred = AnswerFormatter.from_values(
+            entity_values=entity_values,
+            language=language
+        )
+
+        self.assertEqual(
+            answer_pred,
+            output_true
+        )
+
+    @parameterized.expand([
+        ("SOCCER - JAPAN GET LUCKY WIN , CHINA IN SURPRISE DEFEAT .", "Please do NER",
+         "Sentence: SOCCER - JAPAN GET LUCKY WIN , CHINA IN SURPRISE DEFEAT . "
+         "Instruction: Please do NER Options: LOC, PER, MISC"),
+        ("Bitar pulled off fine saves whenever they did .", "Please please do NER",
+         "Sentence: Bitar pulled off fine saves whenever they did . "
+         "Instruction: Please please do NER Options: LOC, PER, MISC")
+    ])
+    def test_format_instance_ner(
+            self,
+            context: str,
+            instruction: str,
+            output_true
+    ):
+
+        entity_values = None
+        entity_spans = None
+
+        options = ["LOC", "PER", "MISC"]
+
+        formatter = NERTaskFormatter()
+        instance_pred = formatter.format_instance(
+            context=context,
+            instruction=instruction,
+            entity_values=entity_values,
+            entity_spans=entity_spans,
+            options=options
+        )
+
+        return self.assertEqual(
+            str(instance_pred),
+            output_true
+        )
+
+    @parameterized.expand([
+        ("SOCCER - JAPAN GET LUCKY WIN , CHINA IN SURPRISE DEFEAT .", "Please do NER",
+         "Sentence: SOCCER - JAPAN GET LUCKY WIN , CHINA IN SURPRISE DEFEAT . "
+         "Instruction: Please do NER: EMPTY Options: LOC, PER, MISC EMPTY is a LOC."),
+        ("Bitar pulled off fine saves whenever they did .", "Please please do NER",
+         "Sentence: Bitar pulled off fine saves whenever they did . "
+         "Instruction: Please please do NER: EMPTY Options: LOC, PER, MISC EMPTY is a LOC.")
+    ])
+    def test_format_instance_type(
+            self,
+            context: str,
+            instruction: str,
+            output_true: str
+    ):
+        entity_values = {"LOC": ["EMPTY"]}
+        entity_spans = None
+
+        options = ["LOC", "PER", "MISC"]
+
+        formatter = EntityTypeTaskFormatter()
+        instance_pred = formatter.format_instance(
+            context=context,
+            instruction=instruction,
+            entity_values=entity_values,
+            entity_spans=entity_spans,
+            options=options
+        )
+
+        return self.assertEqual(
+            str(instance_pred),
+            output_true
+        )
+
+    @parameterized.expand([
+        ("SOCCER - JAPAN GET LUCKY WIN , CHINA IN SURPRISE DEFEAT .", "Please extract entities",
+         "Sentence: SOCCER - JAPAN GET LUCKY WIN , CHINA IN SURPRISE DEFEAT . "
+         "Instruction: Please extract entities EMPTY."),
+        ("Bitar pulled off fine saves whenever they did .", "Please please extract entity words",
+         "Sentence: Bitar pulled off fine saves whenever they did . "
+         "Instruction: Please please extract entity words EMPTY.")
+    ])
+    def test_format_instance_extract(
+            self,
+            context: str,
+            instruction: str,
+            output_true: str
+    ):
+        entity_values = {"LOC": ["EMPTY"]}
+        entity_spans = None
+
+        options = ["LOC", "PER", "MISC"]
+
+        formatter = EntityExtractTaskFormatter()
+        instance_pred = formatter.format_instance(
+            context=context,
+            instruction=instruction,
+            entity_values=entity_values,
+            entity_spans=entity_spans,
+            options=options
+        )
+
+        return self.assertEqual(
+            str(instance_pred),
+            output_true
+        )
