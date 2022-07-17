199d788566a0b37169bb22d4cedaf73bb733fd05
Olga Bystrova <OVlBystrova@sberbank.ru>
2022-07-17 19:26:33 +0300

    add inference draft

--- src/Model.py
+++ src/Model.py
@@ -1,12 +1,11 @@
-from typing import Dict, List
+from typing import Any, Dict, List, Tuple
 
+from dataclasses import astuple
+import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-from src.core.datatypes import TaskType
 from src.formatters import (
-    EntityTypeTaskFormatter,
     NERTaskFormatter,
-    EntityExtractTaskFormatter,
     PredictionSpanFormatter
 )
 
@@ -14,57 +13,60 @@ from src.formatters import (
 class Model:
 
     def __init__(self,
-                 model_path: str,
-                 tokenizer_path: str,
-                 instructions: Dict[str, str],
-                 options: List[str]
+                 model_path_or_name: str,
+                 tokenizer_path_or_name: str
                  ):
 
-        self.tokenizer = T5Tokenizer.from_pretrained(tokenizer_path, local_files_only=True)
-        self.model = T5ForConditionalGeneration.from_pretrained(model_path, local_files_only=True)
+        self.tokenizer = T5Tokenizer.from_pretrained(model_path_or_name,
+                                                     # local_files_only=True
+                                                     )
+        self.model = T5ForConditionalGeneration.from_pretrained(tokenizer_path_or_name,
+                                                                # local_files_only=True
+                                                                )
 
-        self.task_to_formatter = {
-            TaskType.NER: NERTaskFormatter,
-            TaskType.ENTITY_EXTRACTOR: EntityExtractTaskFormatter,
-            TaskType.ENTITY_TYPING: EntityTypeTaskFormatter
-        }
+        self.formatter = NERTaskFormatter()
+
         self.answer_formatter = PredictionSpanFormatter()
 
-        self.instructions = instructions
-        self.options = options
-
-    # TODO add PredictionList formatter for TaskType.EntitiesExtractor
-    # TODO change return to Any task, not only spans
-    def predict(self, text: str, task_type=TaskType.NER):
+    def predict(self,
+                text: str,
+                generation_kwargs: Dict[str, Any],
+                instruction: str,
+                options: List[str]
+                ) -> List[Tuple[int, int, str]]:
         """
         Generate prediction and format spans based on TaskType
+        :param options:
+        :param instruction:
+        :param generation_kwargs:
         :param text: input text
-        :param task_type: one of NER / EntityExtractor / EntityTyping
         :return:
         """
 
-        if task_type not in self.task_to_formatter:
-            raise ValueError(f"Expected task_type to be on of {self.task_to_formatter.keys()}")
+        self.model.eval()
 
-        # TODO this is dirty. Think of more elegant inference
-        data = {"context": text, "entities": []}
-
-        instance = self.task_to_formatter[task_type].format_instance(
-            data=data,
-            instruction=self.instructions[task_type.value],
-            options=self.options
+        instance = self.formatter.format_instance(
+            context=text,
+            instruction=instruction,
+            options=options,
+            entity_spans=None,
+            entity_values=None
         )
 
         input_ids = self.tokenizer(
             [instance.context], [instance.question], return_tensors="pt").input_ids
 
-        outputs = self.model.generate(input_ids)
+        with torch.no_grad():
+            outputs = self.model.generate(input_ids, **generation_kwargs)
+
         # change to false if labels are special tokens
-        answer = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        answer_raw = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
 
         answer_spans = self.answer_formatter.format_answer_spans(
-            instance=instance,
-            prediction=answer
+            context=instance.context,
+            prediction=answer_raw,
+            options=options
         )
+        answer_spans = [astuple(span) for span in answer_spans]
 
         return answer_spans
