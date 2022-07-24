1439052f0a3bfcef69c9b76421daf261f4337f58
Olga Bystrova <OVlBystrova@sberbank.ru>
2022-07-24 11:29:10 +0300

    move save_to_json() to base Reader

--- instruction_ner/core/reader.py
+++ instruction_ner/core/reader.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
+import json
 from pathlib import Path
-from typing import Any, Union
+from typing import Any, Union, List, Dict
+
+from instruction_ner.core.datatypes import DatasetField
 
 
 class Reader(ABC):
@@ -14,3 +17,28 @@ class Reader(ABC):
     @abstractmethod
     def read_from_file(self, path_to_file: Union[str, Path]):
         raise NotImplementedError
+
+    @staticmethod
+    def save_to_json(data: List[Dict[str, Any]], path: Union[str, Path]):
+        """
+        Save processed data to json file
+        :param data: List of sentences with entities
+        :param path: where to save the file
+        :return:
+        """
+
+        if not isinstance(path, str):
+            path = str(path)
+
+        for item in data:
+
+            spans = item[DatasetField.ENTITY_SPANS.value]
+
+            spans_json = [span.to_json() for span in spans]
+            item[DatasetField.ENTITY_SPANS.value] = spans_json
+
+        with open(path, "w", encoding="utf-8") as f:
+            json_string = json.dumps(data, ensure_ascii=False, indent=4)
+            f.write(json_string)
+
+        print(f"Saved to {path}")
--- instruction_ner/readers/conll.py
+++ instruction_ner/readers/conll.py
@@ -1,4 +1,3 @@
-import json
 from pathlib import Path
 from typing import Any, Dict, List, Union, Tuple
 
@@ -42,31 +41,6 @@ class CONLLReader(Reader):
             data_processed.append(dataset_item)
 
         return data_processed
-
-    @staticmethod
-    def save_to_json(data: List[Dict[str, Any]], path: Union[str, Path]):
-        """
-        Save processed data to json file
-        :param data: List of sentences with entities
-        :param path: where to save the file
-        :return:
-        """
-
-        if not isinstance(path, str):
-            path = str(path)
-
-        for item in data:
-
-            spans = item[DatasetField.ENTITY_SPANS.value]
-
-            spans_json = [span.to_json() for span in spans]
-            item[DatasetField.ENTITY_SPANS.value] = spans_json
-
-        with open(path, "w", encoding="utf-8") as f:
-            json_string = json.dumps(data, ensure_ascii=False, indent=4)
-            f.write(json_string)
-
-        print(f"Saved to {path}")
 
     def read_from_file(self, path_to_file: Union[str, Path]):
         """
